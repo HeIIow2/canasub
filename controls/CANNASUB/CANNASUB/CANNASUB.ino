0e6f922ea80798e9f4419d6a53333b7d4ae1bdca
Tim Knöller <tim.knoeller40@gmail.com>
2022-03-09 15:01:08 +0100

    Controls Folder with VS CANNASUB Project

---
+++ controls/CANNASUB/CANNASUB/CANNASUB.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		CANNASUB.ino
+ Created:	3/9/2022 2:52:03 PM
+ Author:	timkn
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
