32852783b7f26a3e09190b0109937529a35be4fc
HeIIow2 <Lars.Noack@outlook.de>
2022-03-30 16:09:45 +0200

    compute avr from measurments collected in a time

--- computation/computation.ino
+++ computation/computation.ino
@@ -1,4 +1,13 @@
 #define SENSOR_NUMBERS 3
+#define MEASURING_TIME 50
+
+// Koordinaten der Sensoren
+// x, y in cm
+int sensor_koordinates[SENSOR_NUMBERS][2] = {
+        {0, 0},
+        {16, 0},
+        {9, 6}
+    };
 
 void setup() 
 {
@@ -6,17 +15,36 @@ void setup()
     Serial.println("Started");
 }
 
+
 void loop() 
 {
-    Serial.print("Reading sensors: ");
-    int sensor_values[SENSOR_NUMBERS];
-    for (int i = 0; i < SENSOR_NUMBERS; i++)
+    static float sensor_values[SENSOR_NUMBERS] {};
     {
-        sensor_values[i] = analogRead(i);
-        Serial.print(sensor_values[i]);
-        Serial.print(" ");
-    }
-    Serial.println();
+        /*
+        Read the sensor values for MEASURING_TIME
+        and stores the average in sensor_values.
+        */
 
-    delay(1000);
+        Serial.print("Reading sensors: ");
+        unsigned long start_measurement = millis();
+        int mesurements = 0;
+
+        while (millis() - start_measurement < MEASURING_TIME)
+        {
+            for (int i = 0; i < SENSOR_NUMBERS; i++)
+            {
+                sensor_values[i] += (float) analogRead(i) / 1023.0;
+            }
+            mesurements++;
+        }
+        
+        for (int i = 0; i < SENSOR_NUMBERS; i++)
+        {
+            sensor_values[i] = sensor_values[i] / mesurements;
+            Serial.print(sensor_values[i]);
+            Serial.print(" ");
+        }
+        Serial.print("mesurements: ");
+        Serial.println(mesurements);
+    }
 }
