736ea3b7f8a5b64307caa9506a1bb94c85961cec
HeIIow2 <Lars.Noack@outlook.de>
2022-03-30 14:45:51 +0200

    computing - read the raw sensor inputs

--- computation/computation.ino
+++ computation/computation.ino
@@ -1,9 +1,22 @@
+#define SENSOR_NUMBERS 3
+
 void setup() 
 {
     Serial.begin(9600);
+    Serial.println("Started");
 }
 
 void loop() 
 {
-    Serial.println("Hello World!");
+    Serial.print("Reading sensors: ");
+    int sensor_values[SENSOR_NUMBERS];
+    for (int i = 0; i < SENSOR_NUMBERS; i++)
+    {
+        sensor_values[i] = analogRead(i);
+        Serial.print(sensor_values[i]);
+        Serial.print(" ");
+    }
+    Serial.println();
+
+    delay(1000);
 }
