9193937fc2fd908350857cc31a34cf3fab9a9aa0
HeIIow2 <Lars.Noack@outlook.de>
2022-04-27 14:52:57 +0200

    computation-added sensor pairs

--- computation/computation.ino
+++ computation/computation.ino
@@ -1,13 +1,17 @@
-#define SENSOR_NUMBERS 3
+#define SENSOR_PAIRS 1
+// in cm
+#define PAIR_DISTANCE 10
 #define MEASURING_TIME 50
 
-// Koordinaten der Sensoren
+
+/*// Koordinaten der Sensoren
 // x, y in cm
-int sensor_koordinates[SENSOR_NUMBERS][2] = {
+static const int sensor_koordinates[SENSOR_NUMBERS][2] = {
         {0, 0},
         {16, 0},
         {9, 6}
-    };
+    };*/
+
 
 void setup() 
 {
@@ -18,7 +22,7 @@ void setup()
 
 void loop() 
 {
-    static float sensor_values[SENSOR_NUMBERS] {};
+    static float sensor_pair_values[SENSOR_PAIRS][2];
     {
         /*
         Read the sensor values for MEASURING_TIME
@@ -31,18 +35,23 @@ void loop()
 
         while (millis() - start_measurement < MEASURING_TIME)
         {
-            for (int i = 0; i < SENSOR_NUMBERS; i++)
+            for (int i = 0; i < SENSOR_PAIRS; i++)
             {
-                sensor_values[i] += (float) analogRead(i) / 1023.0;
+                sensor_pair_values[i][0] += (float) analogRead(i*2);
+                sensor_pair_values[i][1] += (float) analogRead(i*2+1);
             }
             mesurements++;
         }
         
-        for (int i = 0; i < SENSOR_NUMBERS; i++)
+        for (int i = 0; i < SENSOR_PAIRS; i++)
         {
-            sensor_values[i] = sensor_values[i] / mesurements;
-            Serial.print(sensor_values[i]);
+            sensor_pair_values[i][0] = sensor_pair_values[i][0] / mesurements / 1023.0;
+            sensor_pair_values[i][1] = sensor_pair_values[i][1] / mesurements / 1023.0;
+            Serial.print("p" + String(i) + " ");
+            Serial.print(sensor_pair_values[i][0]);
             Serial.print(" ");
+            Serial.print(sensor_pair_values[i][1]);
+            Serial.print("; ");
         }
         Serial.print("mesurements: ");
         Serial.println(mesurements);
