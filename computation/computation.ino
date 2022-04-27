b7d40c81158314bb9dd84ef627fa702c49a87e3d
HeIIow2 <Lars.Noack@outlook.de>
2022-04-27 16:28:41 +0200

    addet way to measure execution time

--- computation/computation.ino
+++ computation/computation.ino
@@ -1,15 +1,6 @@
 #define SENSOR_PAIRS 1
-#define PAIR_SENSOR_DISTANCE 0.5
+#define PAIR_SENSOR_DISTANCE 1.0
 #define MEASURING_TIME 50
-
-
-/*// Koordinaten der Sensoren
-// x, y in cm
-static const int sensor_koordinates[SENSOR_NUMBERS][2] = {
-        {0, 0},
-        {16, 0},
-        {9, 6}
-    };*/
 
 
 void setup() 
@@ -21,6 +12,8 @@ void setup()
 
 void loop() 
 {
+    int start_time = millis();
+
     static float sensor_pair_values[SENSOR_PAIRS][2];
     {
         /*
@@ -63,17 +56,19 @@ void loop()
 
        for (int i = 0; i < SENSOR_PAIRS; i++)
        {
+            // bassierend auf den Schnittpunkt zweier Kreise um die sensoren mit den werten als radius
             // x_light = (-val1^2 + val2^2 - PAIR_SENSOR_DISTANCE^2) / (-2*PAIR_SENSOR_DISTANCE)
             float x_light = (-sensor_pair_values[i][0]*sensor_pair_values[i][0] + sensor_pair_values[i][1]*sensor_pair_values[i][1] - PAIR_SENSOR_DISTANCE*PAIR_SENSOR_DISTANCE) / (-2.0 * PAIR_SENSOR_DISTANCE);
 
             // y_light = -sqrt(x_light^2 - val^2)
-            float y_opt1 = -sqrt(abs(x_light*x_light - sensor_pair_values[i][0]*sensor_pair_values[i][0]));
-            float y_opt2 = -sqrt(abs(x_light*x_light - sensor_pair_values[i][1]*sensor_pair_values[i][1]));
+            float y_opt1 = sqrt(abs(x_light*x_light - sensor_pair_values[i][0]*sensor_pair_values[i][0]));
+            float y_opt2 = sqrt(abs(x_light*x_light - sensor_pair_values[i][1]*sensor_pair_values[i][1]));
             float y_light = (y_opt1 + y_opt2) / 2.0;
             Serial.println("LIGHT(" + String(x_light) + "/" + String(y_light) + ")");
        }
     }
 
-
+    int end_time = millis();
+    Serial.println("Time: " + String(end_time - start_time) + "ms");
     Serial.println("");
 }
