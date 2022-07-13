bd5576236e4a25dc35dacabdb7791b05080b535e
HeIIow2 <Lars.Noack@outlook.de>
2022-07-13 15:35:29 +0200

    cleanup

--- computation/receiver/receiver.ino
+++ computation/receiver/receiver.ino
@@ -20,54 +20,6 @@ void setup()
     Serial.println("Started");
 }
 
-/*
-char float_buff[BYTES_PER_FLOAT]={};
-float read_float() {
-    for (int i=0; i<BYTES_PER_FLOAT; i++) {
-        float_buff[i] = Wire.read();
-    }
-
-    return *(float *)&float_buff;
-}
-
-void on_receive(int number_of_bytes) 
-{
-    if (number_of_bytes < EXPECTED_BYTES) return;
-    /*
-    The Bytes are (in order):
-    X of the blackbox (in cm) 4 bytes - float
-    Y of the blackbox (in cm) 4 bytes - float
-
-
-    Serial.print("X: ");
-    Serial.println(read_float());
-    Serial.print("X: ");
-    Serial.println(read_float());
-
-   // byte x_blackbox = Wire.
-}
-
-
-void read_ic2() 
-{
-    Serial.println(Wire.available());
-    if (Wire.available() >= EXPECTED_BYTES)
-    {
-        if (Wire.available() % EXPECTED_BYTES != 0) {
-            Serial.print("WARNING (I2C): there are ");
-            Serial.print(Wire.available());
-            Serial.print(" bytes availible, where ");
-            Serial.print(EXPECTED_BYTES);
-            Serial.println(" bytes have been expected");
-        }
-
-        Serial.println("available bytes");
-
-        on_receive(Wire.available());
-    }
-
-}
-*/
 
 char float_buff[BYTES_PER_FLOAT]={};
 void loop() 
